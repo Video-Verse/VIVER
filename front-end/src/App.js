e5db613d4e906641f743ccfb63e6a727ddb814f7
jh-baek-95 <86387606+jh-baek-95@users.noreply.github.com>
2023-07-11 23:21:52 +0900

    git test

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './assets/images/logo.png';
 import './App.css';
 import {useEffect, useState} from "react";
 
@@ -21,17 +21,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>viver git test</p>
 
         <ul>
           {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
