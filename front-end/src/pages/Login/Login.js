0d6b1e2a0bfdcee3e03a3c1bc9c64c8b89f41a76
jh-baek-95 <86387606+jh-baek-95@users.noreply.github.com>
2023-07-23 19:33:39 +0900

    login 화면

---
+++ front-end/src/pages/Login/Login.js
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from '../../assets/images/logo.png';
+import './login.css';
+
+// import CommonBtn from './components/button/button';
+
+
+function Login(){
+  return(
+    <div className="wrap">
+        <div className="content">
+            <div className="img-box">
+                <h3 className="login-title">나의 취향 저장소</h3>
+                <img src={logo} className="logo" alt="logo" />
+            </div>
+            <div className="btn-group">
+                <button type="button" className="btn btn-kakao"><span>카카오로 로그인</span></button>
+                <button type="button" className="btn btn-naver"><span>네이버로 로그인</span></button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login;
+
+
+
+
