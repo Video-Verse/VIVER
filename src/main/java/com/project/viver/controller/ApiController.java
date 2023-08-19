2cb85be6f551c486f5314a68a9b3dc2e40d815be
haeun <dl4343@inissoft.com>
2023-08-19 11:59:44 +0900

    kopis api key 추가

---
+++ src/main/java/com/project/viver/controller/ApiController.java
@@ -0,0 +1,13 @@
+package com.project.viver.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+	public Map<String,Object> search (String keyword) {
+		return null;
+	}
+}
