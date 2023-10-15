a8c6eda2fb03b24f07cf1246c9d2b34c15435528
haeun <dl4343@inissoft.com>
2023-10-15 16:31:49 +0900

    musical 수정

--- src/main/java/com/project/viver/service/ApiService.java
+++ src/main/java/com/project/viver/service/ApiService.java
@@ -1,6 +1,8 @@
 package com.project.viver.service;
 
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -28,6 +30,8 @@ public class ApiService {
 	 * @throws ParseException
 	 */
 	public List<Map<String, Object>> search(Map<String, Object> params) throws ParseException {
+		List<Map<String, Object>> result = new ArrayList<>();
+		Map<String,Object> musical = new HashMap<>();
 		// 일배치를 돌린다면 db에서만 가져와도 되지 않을까 하는 맴
 		// 초기 데이터를 많이 넣어놔야 할듯
 
@@ -35,13 +39,15 @@ public class ApiService {
 		// 1. db에 먼저 찾아보기
 		// 뮤지컬
 		List<Map<String, Object>> musicalList = kopisService.getList(params);
+		musical.put("musical", musicalList);
+		result.add(musical);
 
 		// 영화
 		// tmdb 이미지 base url https://image.tmdb.org/t/p/original
 		//
 
 		// 2. 결과가 적으면 api 날려보기
-		kopisService.sendApi(params);
+		//kopisService.sendApi(params);
 		// 3. 그 결과 디비에 업데이트 치기
 
 		// keyword
@@ -52,7 +58,7 @@ public class ApiService {
 		// musical
 		//
 		// 일배치 통해 db에 들어간다 -> db 확인해서 뿌려주기
-		return null;
+		return result;
 	}
 
 }
