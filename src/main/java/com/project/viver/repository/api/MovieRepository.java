ba2fcad2fdc309a1e7bed2f6fa3cd1a64569ff39
haeun <dl4343@inissoft.com>
2023-12-10 15:18:21 +0900

    검색결과 수정

--- src/main/java/com/project/viver/repository/api/MovieRepository.java
+++ src/main/java/com/project/viver/repository/api/MovieRepository.java
@@ -36,17 +36,17 @@ public interface MovieRepository extends JpaRepository<Movie, String>, MovieRepo
 	
 	@Query(value = 
 		    "select "
-		    + "    t.movie_id"                       /*movie_id*/
-		    + "  , t.id "                            /*tmbd_id*/
-		    + "  , t.title "                         /*제목*/
-		    + "  , t.original_title"                 /*원제목*/
-		    + "  , t.adult"                          /*성인유무*/
-		    + "  , t.poster_path"                    /*포스터*/
+		    + "    t.movie_id                as db_id"                       /*movie_id*/
+		    + "  , t.id                      as id"                            /*tmbd_id*/
+		    + "  , t.title                   as title"                         /*제목*/
+		    + "  , t.original_title          as org_title"                 /*원제목*/
+		    + "  , t.adult                   as adult"                          /*성인유무*/
+		    + "  , t.poster_path             as poster"                    /*포스터*/
 		    + " from viver.tb_movie t"
 			+ " where t.del_yn = 'N' and"
-			+ " ( t.title LIKE CONCAT('%', :keyword, '%')"
-			+ " OR t.original_title LIKE CONCAT('%', :keyword, '%'))"
-			+ " Order by t.title asc"
-			+ " limit 10", nativeQuery = true)
+			+ " ( LOWER(t.title) LIKE CONCAT('%', LOWER(:keyword), '%')"
+			+ " OR LOWER(t.original_title) LIKE CONCAT('%', LOWER(:keyword), '%'))"
+			+ " Order by t.id desc"
+			+ " limit 5", nativeQuery = true)
 	List<Map<String,Object>> getSearchList(@Param("keyword") String keyword);
 }
