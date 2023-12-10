211e35d3ceb4b3720dd836dffc2aa4edff9ecdf3
juhui <jhbaek@inissoft.com>
2023-12-10 15:56:00 +0900

    nodata css 수정, app js 수정

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,30 +1,17 @@
-// import logo from './assets/images/logo.png';
-// import logoKr from './assets/images/logo_kr.png'
-//import {useEffect, useState} from "react";
+import React from 'react';
 
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
 import './styles/reset.css';
 
-// import Header from './components/Header/Header';
-import DockBar from './components/DockBar/DockBar';
-// import Search from './pages/Search/Search';
-// import Login from './pages/Login/Login';
-// import Join from './pages/Login/Join';
-import Result from './pages/Search/Result';
+import Login from './pages/Login/Login';
 
 
 function App() {
 
     return (
     <div>
-        {/* <Header /> */}
-        {/* <Login /> */}
-        {/* <Join /> */}
-        {/* <MainSlider/> */}
-            {/* <Search /> */}
-        <Result/>
-        <DockBar />
+        <Login />
     </div>
  
   );
