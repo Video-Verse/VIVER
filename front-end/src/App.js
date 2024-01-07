026f80d4c4990969a373c4b36d5256c1ebc6fb83
juhui <jhbaek@inissoft.com>
2024-01-07 16:22:51 +0900

    앱  진입화면 추가

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
 import './styles/reset.css';
 import Login from './pages/Login/Login';
+import SplashScreen from './pages/Login/SplachScreen';
 
 function App() {
-
+    const [isLoading, setIsLoading] = useState(true);
+    
+    useEffect(() => {
+        // 여기에서 초기 로딩 작업을 수행하고, 작업이 완료되면 setIsLoading(false)를 호출
+    
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 2000); // 2초 후에 로딩 완료
+      }, []);
+    
     return (
-    <div>
+        <div>
+            {isLoading ? (
+        <SplashScreen />
+      ) : (
         <Login />
+      )}
     </div>
  
   );
