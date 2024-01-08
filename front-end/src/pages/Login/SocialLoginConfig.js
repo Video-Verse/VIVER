5671e3dad31d223fa4db25928a971957c7e6b911
juhui <jhbaek@inissoft.com>
2024-01-08 21:21:38 +0900

    naver login

--- front-end/src/pages/Login/SocialLoginConfig.js
+++ front-end/src/pages/Login/SocialLoginConfig.js
@@ -10,16 +10,16 @@ export function socialLoginConfig(userType) {
 	const kakaoRedirectUri = API_BASE_URL + "/oauth/kakao/callback";
 	const naverRedirectUri = API_BASE_URL + "/oauth/naver/callback";
 
-	const naverState = "";
+	const naverState = "state";
 	const responseType = "code";
 
 	// const clientId = "7e6f9285b35e1c5af2ae9e3da753d37e";
 	// const redirectUri = API_BASE_URL + "/oauth/kakao/callback";
 	// const responseType = "code";
 
-	const kakaoUrl = "https://kauth.kakao.com/oauth/authorize?client_id=" + kakaoClientId + "&redirect_uri=" + API_BASE_URL + "/oauth/kakao/callback&response_type=code";
-	const naverUrl = "https://nid.naver.com/oauth2.0/authorize?state=1234&client_id=" + naverClientId + "&redirect_uri=" + API_BASE_URL + " /oauth/naver/callback&response_type=code";
-
+    const kakaoUrl = "https://kauth.kakao.com/oauth/authorize?client_id=" + kakaoClientId + "&redirect_uri=" + API_BASE_URL + "/oauth/kakao/callback&response_type=code";
+    const naverUrl = "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=" + naverClientId + "&state=" + naverState + "&redirect_uri=" + naverRedirectUri;
+    
 	var url = "";
 
 	if (userType == "kakao") {
