aebc03b2eb463016672d78151dedb8d32d1be773
백주희 <juhui@baegjuhuiui-MacBookPro.local>
2024-02-18 15:59:22 +0900

    popup

--- front-end/src/components/button/buttonType2.jsx
+++ front-end/src/components/button/buttonType2.jsx
@@ -3,14 +3,14 @@ import './button.css'
 
 
 const CommonBtnType2 = (props) => {
-  const {onClick, disabled, buttonText1, buttonText2  } = props;
+  const {onClick, disabled, buttonText1, buttonText2, onBtnClick1, onBtnClick2  } = props;
 
   return (
     <div className="btn-group type2">
-        <button type="button" className="btn-second" disabled={disabled} onClick={onClick}>
+        <button type="button" className="btn-second" disabled={disabled} onClick={onBtnClick1}>
             <span>{buttonText1}</span>
         </button>
-        <button type="button" className="btn" disabled={disabled} onClick={onClick}>
+        <button type="button" className="btn" disabled={disabled} onClick={onBtnClick2}>
             <span>{buttonText2}</span>
         </button>
     </div>
